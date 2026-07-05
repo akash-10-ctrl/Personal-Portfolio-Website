0dfd30a921e294d199e6bb1597109e412c92f632
Akash <akash10prince@gmail.com>
2026-07-05 20:13:24 +0530

    Testing Vercel auto deploy

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector(".nav");
 
-menuToggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
-});
+if (menuToggle && nav) {
+    menuToggle.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+}
 
 const filterButtons = document.querySelectorAll(".project_filters button");
 const projectCards = document.querySelectorAll(".project_card");
 
-filterButtons[0].classList.add("active");
-console.log("JS WORKING");
-filterButtons.forEach(button => {
-    button.addEventListener("click", () => {
+if (filterButtons.length > 0) {
+    filterButtons[0].classList.add("active");
 
-        filterButtons.forEach(btn => btn.classList.remove("active"));
-        button.classList.add("active");
+    filterButtons.forEach(button => {
+        button.addEventListener("click", () => {
 
-        const filter = button.dataset.filter;
+            filterButtons.forEach(btn => btn.classList.remove("active"));
+            button.classList.add("active");
 
-        projectCards.forEach(card => {
-            const category = card.dataset.category;
+            const filter = button.dataset.filter;
 
-            if (filter === "all" || filter === category) {
-                card.style.display = "block";
-            } else {
-                card.style.display = "none";
-            }
+            projectCards.forEach(card => {
+                const category = card.dataset.category;
+
+                if (filter === "all" || filter === category) {
+                    card.style.display = "block";
+                } else {
+                    card.style.display = "none";
+                }
+            });
         });
     });
-});
+}
+
+console.log("JS WORKING");
